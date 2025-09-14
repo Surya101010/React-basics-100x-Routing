08da8aaf4b2c55b2de1f0c0229a7857369c18bf1
surya <greatuchsurya@gmail.com>
2025-09-14 18:11:58 +0530

    used navigation and simplified Routes

--- Routing/src/App.jsx
+++ Routing/src/App.jsx
@@ -1,35 +1,60 @@
 
 import './App.css'
-import {BrowserRouter , Routes ,Route, Link} from 'react-router-dom';
+import {BrowserRouter , Routes ,Route, Link, useNavigate, Outlet} from 'react-router-dom';
 function App() {
 
   return (
     <>
       <BrowserRouter>
-      <Link to="/">Allen</Link>
-      |
-      <Link to="/neet/online-coaching-class-11">Class XI</Link>
-      |
-      <Link to="/neet/online-coaching-class-12">Class XII</Link>
       <Routes>
-        <Route path='/neet/online-coaching-class-11' element={<Class11Program />} />
-        <Route path='/neet/online-coaching-class-12' element={<Class12Program />} />
-        <Route path='/' element={<Landing />} />
+        <Route path='/' element={<Layout />}>
+          <Route path='/neet/online-coaching-class-11' element={<Class11Program />} />
+          <Route path='/neet/online-coaching-class-12' element={<Class12Program />} />
+          <Route path='/' element={<Landing />} />
+          <Route path='*' element={<ErrorPage />} />
+        </Route>
       </Routes>
       </BrowserRouter>
     </>
   )
 }
-
+function Layout(){
+  return <div style={{height:"100vh"}}>
+    <Header />
+      <div style={{height:"90vh",backgroundColor:"grey"}}>
+        <Outlet />
+      </div>
+      footer
+  </div>
+}
+function Header(){
+  return<div><Link to="/">Allen</Link>
+      |
+      <Link to="/neet/online-coaching-class-11">Class XI</Link>
+      |
+      <Link to="/neet/online-coaching-class-12">Class XII</Link>
+      </div>
+}
+function ErrorPage(){
+  return <div style={{backgroundColor:"red",textAlign:"center",justifyContent:"center", padding:"4px"}}>
+    Error 404 PAGE Not found
+  </div>
+}
 function Class11Program(){
   return <div>
     Neet Program for class XI
   </div>
 }
 function Class12Program(){
+  const navigate = useNavigate();
+  function redirect(){
+    navigate("/");
+  }
   return <div>
     Neet Program for class XII
+    <div><button onClick={redirect}>Go back to Landing</button>
   </div>
+    </div>
 }
 function Landing(){
   return <div>
